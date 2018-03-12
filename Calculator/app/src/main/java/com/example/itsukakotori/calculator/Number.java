46e099fa72f44e30ac94da567945a81374cc817e
adamadamadamadamadam <adamnurmishwari@gmail.com>
2018-03-12 16:29:11 +0700

    ...

--- Calculator/app/src/main/java/com/example/itsukakotori/calculator/Number.java
+++ Calculator/app/src/main/java/com/example/itsukakotori/calculator/Number.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.itsukakotori.calculator;
 
 import android.graphics.Canvas;
@@ -16,9 +15,9 @@ public class Number extends Vessel {
 
     private double value;
 
-    public Number(double value, float x, float y){
+    public Number(double value, float x, float y) {
         this.value = value;
-        super.middlePoint = new PointF(x,y);
+        super.middlePoint = new PointF(x, y);
         super.type = "number";
     }
 
@@ -30,7 +29,7 @@ public class Number extends Vessel {
         super.b = activity.mCanvas.getHeight() / 20;
         rect.set((int) Math.floor(middlePoint.x - activity.mCanvas.getWidth() / 20), (int) Math.floor(middlePoint.y - activity.mCanvas.getHeight() / 20), (int) Math.floor(middlePoint.x + activity.mCanvas.getWidth() / 20), (int) Math.floor(middlePoint.y + activity.mCanvas.getHeight() / 20));
         paint.setColor(activity.getResources().getColor(R.color.number_color));
-        canvas.drawRect(rect,paint);
+        canvas.drawRect(rect, paint);
         canvas.drawRect(rect, paint);
         paint.setColor(Color.BLACK);
         canvas.drawText(String.valueOf(this.value), middlePoint.x, middlePoint.y, paint);
@@ -42,53 +41,7 @@ public class Number extends Vessel {
         return super.type;
     }
 
-    public double getValue(){
+    public double getValue() {
         return this.value;
     }
 }
-=======
-package com.example.itsukakotori.calculator;
-
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PointF;
-import android.graphics.Rect;
-import android.widget.ImageView;
-
-/**
- * Created by Itsuka Kotori on 3/6/2018.
- */
-
-public class Number extends Vessel {
-
-
-    public Number(String value, float x, float y, MainActivity activity){
-        super.value = value;
-        super.middlePoint = new PointF(x,y);
-        super.type = "number";
-        super.a = activity.mCanvas.getWidth() / 20;
-        super.b = activity.mCanvas.getHeight() / 20;
-    }
-
-    /*@Override
-    void draw(Canvas canvas, ImageView mCanvas, Paint paint, MainActivity activity) {
-        Rect rect = new Rect();
-        //isi method buat bikin rect
-        super.a = activity.mCanvas.getWidth() / 20;
-        super.b = activity.mCanvas.getHeight() / 20;
-        rect.set((int) Math.floor(middlePoint.x - activity.mCanvas.getWidth() / 20), (int) Math.floor(middlePoint.y - activity.mCanvas.getHeight() / 20), (int) Math.floor(middlePoint.x + activity.mCanvas.getWidth() / 20), (int) Math.floor(middlePoint.y + activity.mCanvas.getHeight() / 20));
-        paint.setColor(activity.getResources().getColor(R.color.number_color));
-        canvas.drawRect(rect,paint);
-        canvas.drawRect(rect, paint);
-        paint.setColor(Color.BLACK);
-        canvas.drawText(String.valueOf(this.value), middlePoint.x, middlePoint.y, paint);
-        mCanvas.invalidate();
-    }*/
-
-    @Override
-    String getType() {
-        return super.type;
-    }
-}
->>>>>>> 8aa4119bd339c9e10748876418d6382a5e33b9fb
--- Calculator/app/src/main/java/com/example/itsukakotori/calculator/Operator.java
+++ Calculator/app/src/main/java/com/example/itsukakotori/calculator/Operator.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.itsukakotori.calculator;
 
 import android.graphics.Canvas;
@@ -16,9 +15,9 @@ public class Operator extends Vessel {
 
     private String value;
 
-    public Operator(String value, float x, float y){
+    public Operator(String value, float x, float y) {
         this.value = value;
-        super.middlePoint = new PointF(x,y);
+        super.middlePoint = new PointF(x, y);
         super.type = "operator";
     }
 
@@ -30,7 +29,7 @@ public class Operator extends Vessel {
         super.b = activity.mCanvas.getHeight() / 20;
         rect.set((int) Math.floor(middlePoint.x - super.a), (int) Math.floor(middlePoint.y - super.b), (int) Math.floor(middlePoint.x + super.a), (int) Math.floor(middlePoint.y + super.b));
         paint.setColor(activity.getResources().getColor(R.color.operator_color));
-        canvas.drawRect(rect,paint);
+        canvas.drawRect(rect, paint);
         canvas.drawRect(rect, paint);
         paint.setColor(Color.BLACK);
         canvas.drawText(this.value, middlePoint.x, middlePoint.y, paint);
@@ -42,53 +41,8 @@ public class Operator extends Vessel {
         return super.type;
     }
 
-    public String getValue(){
+    public String getValue() {
         return this.value;
     }
 
 }
-=======
-package com.example.itsukakotori.calculator;
-
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PointF;
-import android.graphics.Rect;
-import android.widget.ImageView;
-
-/**
- * Created by Itsuka Kotori on 3/6/2018.
- */
-
-public class Operator extends Vessel {
-
-    public Operator(String value, float x, float y, MainActivity activity){
-        super.value = value;
-        super.middlePoint = new PointF(x,y);
-        super.type = "operator";
-        super.a = activity.mCanvas.getWidth() / 20;
-        super.b = activity.mCanvas.getHeight() / 20;
-    }
-
-    //@Override
-    /*void draw(Canvas canvas, ImageView mCanvas, Paint paint, MainActivity activity) {
-        Rect rect = new Rect();
-        //isi method buat bikin rect
-        super.a = activity.mCanvas.getWidth() / 20;
-        super.b = activity.mCanvas.getHeight() / 20;
-        rect.set((int) Math.floor(middlePoint.x - super.a), (int) Math.floor(middlePoint.y - super.b), (int) Math.floor(middlePoint.x + super.a), (int) Math.floor(middlePoint.y + super.b));
-        paint.setColor(activity.getResources().getColor(R.color.operator_color));
-        canvas.drawRect(rect,paint);
-        canvas.drawRect(rect, paint);
-        paint.setColor(Color.BLACK);
-        canvas.drawText(this.value, middlePoint.x, middlePoint.y, paint);
-        mCanvas.invalidate();
-    }*/
-
-    @Override
-    String getType() {
-        return super.type;
-    }
-}
->>>>>>> 8aa4119bd339c9e10748876418d6382a5e33b9fb
